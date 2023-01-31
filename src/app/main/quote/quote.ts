6125ba06be8aa58c5689807add218cc39e40a603
chloe.mac-mahon <cmacmahon@oldmutual.com>
2023-01-31 17:40:08 +0200

    Auto-generated commit [ci skip]

--- src/app/main/quote/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-    constructor(public portraitSize: String, public baseCost: number, public amountOfSubjects: number,
-        public frameChosen: String, public frameCost: number, public totalCost : number
-        ){}
-}
